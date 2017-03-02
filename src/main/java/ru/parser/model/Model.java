36242dcfd731f858a2b0835b06b0aac2f7c683c4
set <berserkmc@mail.ru>
2017-03-02 14:12:57 +0300

    работающий парсер для hh.ru

---
+++ src/main/java/ru/parser/model/Model.java
@@ -0,0 +1,32 @@
+package ru.parser.model;
+
+import ru.parser.view.View;
+import ru.parser.vo.Vacancy;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by set on 01.03.17.
+ */
+public class Model {
+    private View view;
+    private Provider[] providers;
+
+    public Model(View view, Provider... providers){
+        if (view==null || providers==null || providers.length==0)
+            throw new IllegalArgumentException();
+        else {
+            this.view = view;
+            this.providers = providers;}
+    }
+
+    public void selectCity(String city) throws IOException {
+        List<Vacancy> vacancies = new ArrayList<>();
+        for (Provider p:providers) {
+                vacancies.addAll(p.getJavaVacancies(city));
+        }
+        view.update(vacancies);
+    }
+}
